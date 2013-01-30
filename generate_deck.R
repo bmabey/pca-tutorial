fefe5843267e3a896bb3017771ed65c1191d7672
Ben Mabey <ben@benmabey.com>
2013-01-30 10:33:26 -0700

    cleans up cruft from generate_deck.R

--- generate_deck.R
+++ generate_deck.R
@@ -1,11 +1,3 @@
 #!/usr/bin/env Rscript
 library(slidify)
 slidify("index.Rmd", "index.html")
-#library(markdown)
-#argv = commandArgs(trailingOnly = TRUE)
-#call_logs_file = argv[1]
-#output = sub("tsv", "md",
-#             sub("call_logs", "report", call_logs_file))
-#knit("report.Rmd", output=output)
-#markdownToHTML(output,  sub("md", "html", output))
-#cat(paste("HTML Output: ", sub("md", "html", output), "\n"))
